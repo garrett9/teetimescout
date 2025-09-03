a5378ef2693da32ca5f83da3a9bd7de34ac51ae2
Garrett S <garrettshevach@gmail.com>
2025-09-03 03:20:23 +0000

    Block bots

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\BlockCloudProviders;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,7 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware
             ->redirectGuestsTo('/')
-            ->statefulApi();
+            ->statefulApi()
+            ->append([
+                BlockCloudProviders::class,
+            ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
